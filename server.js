b761875026f108cb079707734c4867edc52bd570
Bairon29 <baironvasquez29@gmail.com>
2019-01-30 11:02:05 -0500

    Stable point of the nodejs server, starting client size with react

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var app = express();
+var mongoose = require('mongoose');
+var port = process.env.PORT || 5000;
+
+var users = require('./routes/Users');
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+);
+
+const mongoURI = 'mongodb://localhost:27017/mernloginreg';
+
+mongoose.connect(mongoURI, {useNewUrlParser: true})
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
+
+app.use('/users', users);
+
+app.listen(port, () => {
+	console.log("Server is running on port: " + port);
+});
